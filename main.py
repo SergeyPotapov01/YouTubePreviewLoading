f09e0b25bdf8bad37b37d664f38086f9d5f3a4d7
SergeyPotapov01 <88240890+SergeyPotapov01@users.noreply.github.com>
2021-11-24 20:46:54 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,11 +4,20 @@ import requests
 from PIL import Image
 
 
+def _get_url(url: str):
+    if url[0:32] == 'https://www.youtube.com/watch?v=':
+        return url[32:43]
+    if url[0:24] == 'www.youtube.com/watch?v=':
+        return url[24:35]
+    if len(url) == 11:
+        return url
+
 def save_preview(url: str, i: str):
+    url = _get_url(url)
     im = requests.get(f'https://i.ytimg.com/vi_webp/{url}/maxresdefault.webp').content
     im = Image.open(io.BytesIO(im)).convert('RGB')
-    im.save(f'{i}.jpg', 'jpeg')
+    im.save(f'img/{i}.jpg', 'jpeg')
 
 if __name__ == '__main__':
-    for i in range(100):
-        save_preview(input('Введи ссыль на видос: '), str(i))
+    for i in range(256):
+        save_preview(input('Введи ссылку на на видео: '), str(i))
